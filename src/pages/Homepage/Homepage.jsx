2ed0ffde4f692097ae1e843c109adb369628e19a
Jeonha Kang <kangjeonha@gmail.com>
2024-08-03 05:24:26 +0900

    third-commit

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Banner from './components/Banner/Banner'
+import PopularMoviesSlide from './components/Banner/PopularMoviesSlide/PopularMoviesSlide'
 
 //1. 배너 => popular 영화를 들고와서 첫번째 아이템을 보여주자
 //2. popular movie
@@ -7,7 +8,10 @@ import Banner from './components/Banner/Banner'
 //4. upcoming movie
 const Homepage = () => {
   return (
-    <div><Banner /></div>
+    <div>
+      <Banner />
+      <PopularMoviesSlide />
+    </div>
   )
 }
 
